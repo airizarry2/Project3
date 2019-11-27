b20709a385b50b36fcc3dbe1fef337741f2417f1
airizarry2 <54568019+airizarry2@users.noreply.github.com>
2019-11-26 19:05:36 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -32,11 +32,34 @@ function park() {
 
 
 }
-function slider (){
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+	var i;
 
 
+	var slides = document.getElementsByClassName("imgfade");
 
-	
+
+	for (i = 0; i < slides.length; i++) {
+
+		slides[i].style.display = "none";
+	}
+
+	slideIndex++;
+
+
+	if (slideIndex > slides.length) {
+		slideIndex = 1;
+	}
+
+
+	slides[slideIndex - 1].style.display = "block";
+
+
+	setTimeout(showSlides, 3000);
 }
 
 window.addEventListener('load', park);
